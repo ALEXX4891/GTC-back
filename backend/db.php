595338397aa21ab57a1d2b02142319106f52ce7d
il115 <web@ww.net.ru>
2024-10-14 16:02:11 +0500

    подключит БД, добавит вывод новостей

--- backend/db.php
+++ backend/db.php
@@ -1,4 +1,4 @@
-<?php
+<?
 error_reporting(0);
 
 $host = 'localhost'; // имя хоста
@@ -7,13 +7,3 @@ $user = 'wwserver_tgc'; // имя пользователя
 $pswd = 'mfKZwSrad6f%'; //пароль
 
 $db = mysqli_connect($host, $user, $pswd, $database) or die("Ошибка БД localhost #1");
-
-// ///////////////
-// $db = mysqli_connect("localhost", "my_user", "my_password", "world");
-
-// $city = "'s-Hertogenbosch";
-
-// /* этот запрос с экранированным $city будет работать */
-// $query = sprintf("SELECT CountryCode FROM City WHERE name='%s'",
-//     mysqli_real_escape_string($db, $city));
-// // $result = mysqli_query($db, $query);
--- pages/novosti/index.php
+++ pages/novosti/index.php
@@ -5,6 +5,8 @@ $keywords = '';
 $page = 'news-page';
 include '' . $_SERVER["DOCUMENT_ROOT"] . '/includes/head.php';
 include '' . $_SERVER["DOCUMENT_ROOT"] . '/includes/header.php';
+$result = mysqli_query($db, "SELECT * FROM news WHERE status = 1 ORDER BY DATE DESC");
+$row = mysqli_fetch_array($result);
 ?>
 
 <main class="main">
@@ -82,6 +84,69 @@ include '' . $_SERVER["DOCUMENT_ROOT"] . '/includes/header.php';
             </div>
 
           </li>
+
+          <?
+          // $result = mysqli_query($db, "SELECT * FROM apartments WHERE id = " . $_GET['id']);
+          $result = mysqli_query($db, "SELECT * FROM news WHERE status = 1 ORDER BY DATE DESC");
+
+          $row = mysqli_fetch_array($result);
+
+          // if ($row == '') {
+          //   echo 'Ничего не нашлось';
+          // }
+
+          // echo '<pre>';
+          // print_r($row);
+          // echo '</pre>';
+
+          // Сосновый | ГП 8 | 1 / 2 этаж
+
+          if (mysqli_num_rows($result) > 0) {
+            do {
+              echo '
+                <li class="news-page__cards-item">
+                <div class="news-page__card-img-wrapper">
+                  <img src="/assets/img/' . $row['photo'] . '" alt="' . $row['title'] . '">
+                </div>
+                <p class="news-page__card-date">
+                ' . date("d.m.Y", strtotime($row['date'])) . '
+                </p>
+                <a class="news-page__card-link" href="/pages/novosti-item/?id=' . $row['id'] . '">
+                  <h2 class="news-page__card-title">
+                  ' . $row['title'] . '
+                  </h2>
+                </a>
+              </li>
+
+
+
+              <li class="news-page__item">
+              <div class="news-page__item-img">
+                <picture>
+                  <source srcset="/assets/img/news-img-1.webp" type="image/webp"><img src="/assets/img/news-img-1.jpg"
+                    alt="">
+                </picture>
+              </div>
+              <div class="news-page__item-text-wrapper">
+                <h2 class="news-page__item-title">
+                  Система контроля и учёта топлива – экономия в режиме реального времени
+                </h2>
+                <p class="news-page__item-text">
+                  Оборудование контроля топлива «СКАУТ» позволяет собирать и анализировать данные в реальном времени и
+                  формировать нужные руководителю предприятия отчеты
+                </p>
+                <a class="news-page__item-btn btn btn_trans-orange" href="novosti-item.html">
+                  Подробнее
+                </a>
+              </div>
+  
+            </li>
+
+
+                ';
+            } while ($row = mysqli_fetch_array($result));
+          }
+          ?>
 
         </ul>
 
